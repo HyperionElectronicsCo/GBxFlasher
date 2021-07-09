e51c3d93b043beb16abc4bd52c0f43222c21201c
HyperionElectronicsCo <HyperionElectronicsCo@hotmail.com>
2021-07-09 19:46:40 +0100

    Works

---
+++ app/src/main/java/com/mycompany/gbxflasher/DisplayNameAndSize.java
@@ -0,0 +1,14 @@
+package com.mycompany.gbxflasher;
+
+public class DisplayNameAndSize {
+
+    /**
+     * The display name as depicted by {@link android.provider.OpenableColumns#DISPLAY_NAME}
+     */
+    public String displayName;
+
+    /**
+     * Size of the file or content in bytes
+     */
+    public long size;
+}
--- app/src/main/java/com/mycompany/gbxflasher/MainActivity.java
+++ app/src/main/java/com/mycompany/gbxflasher/MainActivity.java
@@ -1,16 +1,20 @@
 package com.mycompany.gbxflasher;
 
 import android.Manifest;
+import android.app.Activity;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.hardware.usb.UsbDevice;
 import android.hardware.usb.UsbDeviceConnection;
 import android.hardware.usb.UsbManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -26,12 +30,13 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
-	private static final String TAG = "UsbHost";
 	TextView mFw;
 	TextView Tv1;
+	TextView fSelect;
 	TextView mDeviceText;
 	UsbManager mUsbManager;
 	UsbDevice mDevice;
@@ -40,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
 	Button mConnect;
 	Button mReadInfo;
 	Button mReadRom;
-	Button mWriteRom;
+	//Button mWriteRom;
 	Button mBackupSave;
 	Button mRestoreSave;
 
@@ -49,165 +54,43 @@ public class MainActivity extends AppCompatActivity {
 	private static final int REQ_VALUE = 0x200;
 	private static final int REQ_INDEX = 0x00;
 	private static final int LENGTH = 64;
-
+	
+	private static final String TAG = "UsbHost";
+	private BroadcastReceiver detachReceiver;
 	private String path;
-
+	
 	String[] permissions = new String[]{
         Manifest.permission.READ_EXTERNAL_STORAGE,
         Manifest.permission.WRITE_EXTERNAL_STORAGE,
 	};
 
-	private BroadcastReceiver detachReceiver;
+	private static final int PICKFILE_RESULT_CODE = 0;
+
+	private Uri uri;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		Tv1 = (TextView) findViewById(R.id.Tv1);
-		mDeviceText = (TextView) findViewById(R.id.text_status);
-		mFw = (TextView) findViewById(R.id.mFw);
-
-		mConnect = (Button) findViewById(R.id.button_connect);
-		mReadInfo = (Button) findViewById(R.id.read_Info);
-		mReadRom = (Button) findViewById(R.id.read_Rom);
-		mWriteRom = (Button) findViewById(R.id.write_Rom);
-		mBackupSave = (Button) findViewById(R.id.backup_Save);
-		mRestoreSave = (Button) findViewById(R.id.restore_Save);
-
+		
+		path = ("/storage/3AD8-565D/");
+		
+		Tv1 = findViewById(R.id.Tv1);
+		mFw = findViewById(R.id.mFw);
+		fSelect = findViewById(R.id.fSelect);
+		mDeviceText = findViewById(R.id.text_status);
+		mConnect = findViewById(R.id.button_connect);
+		mReadInfo = findViewById(R.id.read_Info);
+		mReadRom = findViewById(R.id.read_Rom);
+		//mWriteRom = findViewById(R.id.write_Rom);
+		mBackupSave = findViewById(R.id.backup_Save);
+		mRestoreSave = findViewById(R.id.restore_Save);
+		
 		mUsbManager = (UsbManager) getSystemService(Context.USB_SERVICE);
 
-
-		path = ("/storage/3AD8-565D/");
-
-
-
-		mReadInfo.setOnClickListener(new OnClickListener() {
-				public void onClick(View v) {
-					// TODO Auto-generated method stub
-					
-				}
-			});
-		mReadRom.setOnClickListener(new OnClickListener() {
-				public void onClick(View v) {
-					// TODO Auto-generated method stub
-				}
-			});
-		mWriteRom.setOnClickListener(new OnClickListener() {
-				public void onClick(View v) {
-					// TODO Auto-generated method stub
-				}
-			});
-		mBackupSave.setOnClickListener(new OnClickListener() {
-				public void onClick(View v) {
-					// TODO Auto-generated method stub
-				}
-			});
-		mRestoreSave.setOnClickListener(new OnClickListener() {
-				public void onClick(View v) {
-					// TODO Auto-generated method stub
-				}
-			});
-
-
-
-		detachReceiver = new BroadcastReceiver() {
-			public void onReceive(Context context, Intent intent) {
-				if (intent.getAction().equals(UsbManager.ACTION_USB_DEVICE_DETACHED))
-					stopSelf();
-			}
-
-			private void stopSelf() {
-				mDeviceText.append("\n" + "Disconnected");
-				Tv1.setBackgroundResource(R.drawable.ic_offline);
-				mFw.setText("N/A");
-			}
-		};
-
-		IntentFilter filter = new IntentFilter();
-		filter.addAction(UsbManager.ACTION_USB_DEVICE_DETACHED);
-		registerReceiver(detachReceiver, filter);
-
-	}
-	private boolean checkPermissions() {
-		int result;
-		List<String> listPermissionsNeeded = new ArrayList<>();
-		for (String p : permissions) {
-			result = ContextCompat.checkSelfPermission(this, p);
-			if (result != PackageManager.PERMISSION_GRANTED) {
-				listPermissionsNeeded.add(p);
-			}
+		
+	
 		}
-		if (!listPermissionsNeeded.isEmpty()) {
-			ActivityCompat.requestPermissions(this, listPermissionsNeeded.toArray(new String[listPermissionsNeeded.size()]), 100);
-			return false;
-		}
-		return true;
-	}
-	@Override
-	public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-		if (requestCode == 100) {
-			if (grantResults.length > 0
-                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-				// do something
-
-
-			}
-			return;
-		}
-	}
-
-	private void read() {
-		//Read FW Info T-DRIVER.DMP
-		StringBuilder text = new StringBuilder();
-		try {
-
-			File file = new File(path, "T-DRIVER.DMP");
-
-			BufferedReader br = new BufferedReader(new FileReader(file));  
-			String line;   
-			while ((line = br.readLine()) != null) {
-                text.append(line);
-                text.append('\n');
-			}
-			br.close() ;
-		} catch (IOException e) {
-			e.printStackTrace();           
-		}
-
-
-		mFw.setText(text.toString()); ////Set the text to text view.
-	}
-
-	@Override
-	protected void onRestart() {
-		super.onRestart();
-		recreate();
-	}
-
-	@Override
-	protected void onResume() {
-		super.onResume();
-
-		mPermissionIntent = PendingIntent.getBroadcast(this, 0, new Intent(ACTION_USB_PERMISSION), 0);
-		IntentFilter filter = new IntentFilter(ACTION_USB_PERMISSION);
-		registerReceiver(mUsbReceiver, filter);
-		updateDeviceList();
-	}
-
-	@Override
-	protected void onPause() {
-		super.onPause();
-		unregisterReceiver(mUsbReceiver);
-	}
-
-	public void onConnectClick(View v) {
-		if (mDevice == null) {
-			return;
-		}
-		mUsbManager.requestPermission(mDevice, mPermissionIntent);
-	}
-
-
 	private static final String ACTION_USB_PERMISSION = "com.mycompany.gbxflasher.USB_PERMISSION";
 	private final BroadcastReceiver mUsbReceiver = new BroadcastReceiver() {
 		public void onReceive(Context context, Intent intent) {
@@ -225,8 +108,6 @@ public class MainActivity extends AppCompatActivity {
 			}
 		}
 	};
-
-
 	private void getDeviceStatus(UsbDevice device) {
 		UsbDeviceConnection connection = mUsbManager.openDevice(device);
 		byte[] buffer = new byte[LENGTH];
@@ -234,6 +115,7 @@ public class MainActivity extends AppCompatActivity {
 								   buffer, LENGTH, 2000);
 		connection.close();
 	}
+
 	private void updateDeviceList() {
 		HashMap<String, UsbDevice> connectedDevices = mUsbManager
             .getDeviceList();
@@ -245,11 +127,123 @@ public class MainActivity extends AppCompatActivity {
 			for (UsbDevice device : connectedDevices.values()) {
 				mDevice = device;
 			}
+			Handler handler = new Handler();
+			handler.postDelayed(new Runnable() {
+					public void run() {
+						// Actions to do after 5 seconds
+						
 			mDeviceText.setText("USB Device connected");
 			mConnect.setEnabled(true);
 			Tv1.setBackgroundResource(R.drawable.ic_online);
-			checkPermissions();
 			read();
+					}
+				}, 2000);
+		}
+		
+		detachReceiver = new BroadcastReceiver() {
+			public void onReceive(Context context, Intent intent) {
+				if (intent.getAction().equals(UsbManager.ACTION_USB_DEVICE_DETACHED))
+					stopSelf();
+			}
+
+			private void stopSelf() {
+				mDeviceText.setText("USB Device Disconnected");
+				Tv1.setBackgroundResource(R.drawable.ic_offline);
+				mFw.setText("N/A");
+			}
+		};
+		
+		IntentFilter filter = new IntentFilter();
+		filter.addAction(UsbManager.ACTION_USB_DEVICE_DETACHED);
+		registerReceiver(detachReceiver, filter);
+	}
+
+	private void read() {
+		//Read FW Info T-DRIVER.DMP
+		StringBuilder text = new StringBuilder();
+		try {
+			File file = new File(path, "T-DRIVER.DMP");
+
+			BufferedReader br = new BufferedReader(new FileReader(file));  
+			String line;   
+			while ((line = br.readLine()) != null) {
+                text.append(line);
+                text.append('\n');
+			}
+			br.close() ;
+		} catch (IOException e) {
+			e.printStackTrace();           
+		}
+		mFw.setText(text.toString()); 
+	}
+	
+	
+
+	/* public void onConnectClick(View v) {
+		mUsbManager.requestPermission(mDevice, mPermissionIntent);
+		if (mDevice == null) {
+			return;
+		}
+		mUsbManager.requestPermission(mDevice, mPermissionIntent);
+	}
+	*/
+	public void writeROM(View v)
+    {
+        Intent chooseFile = new Intent(Intent.ACTION_GET_CONTENT);
+        chooseFile.addCategory(Intent.CATEGORY_OPENABLE);
+        chooseFile.setType("*/*");
+        startActivityForResult(
+            Intent.createChooser(chooseFile, "Choose a file"),
+            PICKFILE_RESULT_CODE
+        );
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICKFILE_RESULT_CODE && resultCode == Activity.RESULT_OK){
+            String path = new File(data.getData().getPath()).getAbsolutePath();
+
+            if(path != null){
+                uri = data.getData();
+
+                String filename;
+                Cursor cursor = getContentResolver().query(uri,null,null,null,null);
+
+                if(cursor == null) filename=uri.getPath();
+                else{
+                    cursor.moveToFirst();
+                    int idx = cursor.getColumnIndex(MediaStore.Files.FileColumns.DISPLAY_NAME);
+                    filename = cursor.getString(idx);
+                    cursor.close();
+                }
+
+                String name = filename.substring(0,filename.lastIndexOf("."));
+                String extension = filename.substring(filename.lastIndexOf(".")+1);
+                fSelect.setText(name + extension);
+            }
 		}
 	}
+	@Override
+	protected void onResume() {
+		super.onResume();
+
+		mPermissionIntent = PendingIntent.getBroadcast(this, 0, new Intent(ACTION_USB_PERMISSION), 0);
+		IntentFilter filter = new IntentFilter(ACTION_USB_PERMISSION);
+		registerReceiver(mUsbReceiver, filter);
+		updateDeviceList();
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		unregisterReceiver(mUsbReceiver);
+	}
 }
+
+	
+
+	
+
+
+	
+
